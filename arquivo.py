1814616b5660626b54efdfee8b4e300609b8ec20
Samuel Starts <samuel@starts.com>
2023-01-21 17:11:16 -0300

    arquivo fix

--- arquivo.py
+++ arquivo.py
@@ -1,9 +1,32 @@
 
+#Script teste
 import mcpi.minecraft as minecraft
+import time
 mc = minecraft.Minecraft.create()
 
-x = 16
-y = 1
-z = -5
-#mc.postToChat("Hello world")
-mc.player.setTilePos(x, y, z)
+# Teleport
+#mc.player.setTilePos(x, y, z)
+
+# Digging a crater where you want
+x = 50
+z = -14
+y = 75
+
+largura = 100
+profundidade = 64
+
+strl = "Starting stone removal... y:",profundidade," xz²",largura
+# Communicating to the user what is being started
+mc.postToChat(strl)
+
+# Starting quarry action
+for y_1 in range(y, y-profundidade, -1):
+    for x_1 in range(x,x+largura,1):
+        for z_1 in range(z,z+largura,1):
+            time.sleep(0.01)# velocity
+            print(x_1," ", y_1," ", z_1)
+            mc.setBlock(x_1, y_1, z_1,0)
+
+
+
+
